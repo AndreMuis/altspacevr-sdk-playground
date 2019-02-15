29204cc344b3353b1a640b329de9f0653d366050
Andre Muis <Andre@AndreMuis.com>
2019-02-14 17:16:26 -0800

    changes

--- built/server.js
+++ built/server.js
@@ -9,16 +9,16 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const mixed_reality_extension_sdk_1 = require("@microsoft/mixed-reality-extension-sdk");
 const path_1 = require("path");
-const plane_tiling_1 = __importDefault(require("./plane-tiling"));
+const app_1 = __importDefault(require("./app"));
 process.on('uncaughtException', err => console.log('uncaughtException', err));
 process.on('unhandledRejection', reason => console.log('unhandledRejection', reason));
 // Start listening for connections, and serve static files
 const server = new mixed_reality_extension_sdk_1.WebHost({
     baseDir: path_1.resolve(__dirname, '../public')
 });
-// server.adapter.onConnection(context => new Demo(context, server.baseUrl));
+server.adapter.onConnection(context => new app_1.default(context, server.baseUrl));
 // server.adapter.onConnection(context => new EaseCurve(context, server.baseUrl));
 // server.adapter.onConnection(context => new LookAt(context, server.baseUrl));
-server.adapter.onConnection(context => new plane_tiling_1.default(context, server.baseUrl));
+// server.adapter.onConnection(context => new PlaneTiling(context, server.baseUrl));
 exports.default = server;
 //# sourceMappingURL=server.js.map
--- src/server.ts
+++ src/server.ts
@@ -21,9 +21,9 @@ const server = new WebHost({
     baseDir: resolvePath(__dirname, '../public')
 });
 
-// server.adapter.onConnection(context => new Demo(context, server.baseUrl));
+server.adapter.onConnection(context => new Demo(context, server.baseUrl));
 // server.adapter.onConnection(context => new EaseCurve(context, server.baseUrl));
 // server.adapter.onConnection(context => new LookAt(context, server.baseUrl));
-server.adapter.onConnection(context => new PlaneTiling(context, server.baseUrl));
+// server.adapter.onConnection(context => new PlaneTiling(context, server.baseUrl));
 
 export default server;
