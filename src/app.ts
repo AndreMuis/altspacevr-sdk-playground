5c04545e7ed35e051fb7288f3bf86dfc95bb07f8
Andre Muis <Andre@AndreMuis.com>
2019-03-17 14:47:35 -0700

    changes

--- src/app.ts
+++ src/app.ts
@@ -486,7 +486,7 @@ export default class Demo {
                         pitch: note
                     });
 
-                await this.delay(300);
+                await this.delay(500);
             }
         };
         notesButtonBehavior.onClick('released', playNotes);
