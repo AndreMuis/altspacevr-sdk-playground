405a4960bca38dfe1d8423f254d9ad9a3567a698
Andre Muis <Andre@AndreMuis.com>
2019-10-01 15:29:28 -0700

    Update many-objects.ts

--- src/many-objects.ts
+++ src/many-objects.ts
@@ -16,18 +16,23 @@ export default class ManyObjects {
     private setupSphereActors()
     {
         for (let x = 1; x <= 200; x = x + 1) {
-            MRESDK.Actor.Create(this.context, {
-                actor: {
-                    appearance: {
-                        meshId: this.assetContainer.createSphereMesh('sphere', 0.4, 10, 10).id
-                    },
-                    transform: {
-                        local: {
-                            position: {x: x, y: 1, z: 0}
+            try {
+                MRESDK.Actor.Create(this.context, {
+                    actor: {
+                        appearance: {
+                            meshId: this.assetContainer.createSphereMesh('sphere', 0.4, 10, 10).id
+                        },
+                        transform: {
+                            local: {
+                                position: {x: x, y: 1, z: 0}
+                            }
                         }
                     }
-                }
-            })
+                })
+            }
+            catch (e) {
+                console.log(e)
+            }
         }
     }
 }
