787a9161e84dfde4ef1954cce9c08f9e14c33dad
Andre Muis <Andre@AndreMuis.com>
2019-10-01 18:35:30 -0700

    Update many-objects.ts

--- src/many-objects.ts
+++ src/many-objects.ts
@@ -6,7 +6,7 @@ export default class ManyObjects {
     constructor(private context: MRESDK.Context, private baseUrl: string) {
         this.assetContainer = new MRESDK.AssetContainer(this.context)
 
-        this.context.onStarted(() => this.setupSphereActors())
+        this.context.offUserJoined(() => this.setupSphereActors())
     }
     
     private setupSphereActors()
